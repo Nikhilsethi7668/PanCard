8bc9a022dae3dd72393cfce8755f1b52dc31ef3b
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2025-04-08 18:06:28 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("PanEmailDB", "root", "India_123", {
-  host: "66.94.120.78", // IP address of your Ubuntu server
+  host: "94.250.203.249", // IP address of your Ubuntu server
   dialect: "mysql",
   port: 3306, // Default MySQL port
   logging: false, // Disable logging for cleaner output
