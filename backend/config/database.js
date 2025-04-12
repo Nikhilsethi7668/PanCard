86496787e6637105ef5ac9d7fd75a744654328a7
Adarsh <itsadarsh33@gmail.com>
2025-04-12 16:13:50 +0530

    Fix

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("PanEmailDB", "root", "Nikhil62642$$", {
+const sequelize = new Sequelize("PanEmailDb", "root", "India_123", {
   host: "localhost", // IP address of your Ubuntu server
   dialect: "mysql",
   port: 3306, // Default MySQL port
